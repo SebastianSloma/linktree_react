242334b5e6f1d27d5507d44f38a2587a8a234233
Sebastian <sebastiansloma@gmail.com>
2023-07-06 12:00:10 +0200

    import react icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import profilePic from'./merida.png'
+import { FaGithub, FaInstagram, FaTelegram, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 function App() {
   return (
