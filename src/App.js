9802d6ce6ab5ede4b72564c6c85cc566d65e97a5
Sebastian <sebastiansloma@gmail.com>
2023-07-06 12:11:05 +0200

    added const links

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import profilePic from'./merida.png'
 import { FaGithub, FaInstagram, FaTelegram, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 function App() {
+
+  const links =[
+
+    {
+      url: 'http://www.youtube.com/',
+      title: 'YouTube',
+      icon: <FaYoutube/>
+    },
+    {
+      url: 'http://www.github.com/',
+      title: 'Github',
+      icon: <FaGithub/>
+    },
+    {
+      url: 'http://www.instagram.com/',
+      title: 'Instagram',
+      icon: <FaInstagram/>
+    },
+    {
+      url: 'http://www.Twitter.com/',
+      title: 'Twitter',
+      icon: <FaTwitter/>
+    },
+    {
+      url: 'http://www.telegram.com/',
+      title: 'Telegam',
+      icon: <FaTelegram/>
+    },
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={profilePic} className="App-logo" alt="profilepic" />
+        
       </header>
     </div>
   );
